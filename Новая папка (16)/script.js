3f7a7f896d8cc21e17d1b966976d5978a93180f3
woid296 <stemsolo4@gmail.com>
2024-07-27 18:10:17 +0500

    Add files via upload

---
+++ Новая папка (16)/script.js
@@ -0,0 +1,36 @@
+// let ball=50;
+// let javob=ball>60 ? 'otdi': 'otmadi'
+//   console.log('imtixon javobingiz' , javob)        
+
+// let result=Fizzbuzz(15)
+// console.log(result);
+// function Fizzbuzz(input){
+//     if(typeof input !=='number'){
+//         return 'son emas'
+//     }
+//     if(input %3 %5 ===0){
+//         return 'Fizzbuzz'
+//     }
+//     if(input % 3 ===0){
+//         return 'Fizz'
+//     }
+//     if(input % 5 ===0){
+//         return 'Buzz'
+//     }
+   
+// }
+
+let color1='oq'
+let color2='qora'
+
+
+
+let temp = color1;
+color1 = color2;
+color2 = temp;
+
+console.log(color1); 
+console.log(color2); 
+
+
+
